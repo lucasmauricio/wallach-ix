1d41066fa9897781d5036b645fe3f721a0f1d42e
lucasmauricio <lucasmauriciocmartins@gmail.com>
2016-12-08 10:52:59 -0200

    WIP changing console output to logging module instead of print command.

--- collection-service.py
+++ collection-service.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 import requests
 import socket
 import os
+import logging
 
 __all__ = ['make_json_app']
 
@@ -34,7 +35,7 @@ def department_api(department_id=None):
     elif request.method == "POST":
         if request.is_json:
             # se o request veio com o mimetype "json" usa os dados para inserir novas noticias
-            noticias.bulk_insert(request.json)
+            departments.bulk_insert(request.json)
             return "Department created successfully", 201
 
     elif request.method == "DELETE":
@@ -45,46 +46,47 @@ def department_api(department_id=None):
 
 #registrando API do serviço
 def api_register(api_id, api_data):
-    print "registrando o serviço '{}'".format(api_id)
+    logging.debug("registrando o serviço '{}'".format(api_id))
 
     REGISTRADOR_API = 'http://registrator-serv:8080/asset/'
     headers = {'Content-Type': 'application/json'}
     r = requests.put(REGISTRADOR_API+api_id, headers=headers, json=api_data)
     if r.status_code == 201:
-        print " -registrado com sucesso: {}".format(r.status_code)
+        logging.debug(" -registrado com sucesso: {}".format(r.status_code))
     else:
-        print " -ocorreu erro no registro do serviço: {}".format(r.status_code)
-        print " ->resposta do registrador: " + r.text
+        logging.error(" -ocorreu erro no registro do serviço: {}".format(r.status_code))
+        logging.debug(" ->resposta do registrador: " + r.text)
 
 
 def run_server(port):
-    print ""
-    print ""
-    print "initializing the app and its 2 services"
+    logging.debug("")
+    logging.info("initializing the app and its 2 services")
 #    print "the server's address is " + app.config['SERVER_NAME']
-    print app.config.get('SERVER_NAME')
-    print app.config.get('PORT')
+    logging.debug(app.config.get('SERVER_NAME'))
+    logging.debug(app.config.get('PORT'))
 
     #registrando serviço enterprise_api
     enterprise_api_id = 'enterprise'
     #TODO automatizar a forma de recuperar o endereço do serviço
     payload = {'name':'Enterprise data', "address": "http://{}:{}/enterprise".format('localhost', port)}
-    print payload
+    logging.debug(payload)
     api_register(enterprise_api_id, payload);
 
     #registrando serviço departments_api
     departments_api_id = 'department'
     #TODO automatizar a forma de recuperar o endereço do serviço
     payload = {'name':'Departments data', "address": "http://{}:{}/department".format('localhost', port)}
-    print payload
+    logging.debug(payload)
     api_register(departments_api_id, payload);
 
     app.run(host= '0.0.0.0', port=port, debug=True, use_reloader=False)
 
 
 if __name__ == '__main__':
-    #TODO fazer esse log funcionar
-    app.logger.info("initializing the app")
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s (%(threadName)-6s) %(message)s',
+                        )
+    logging.info("initializing the app")
 
     host_port = int(os.environ.get('PORT', 8000))
 
@@ -92,7 +94,7 @@ if __name__ == '__main__':
     #TODO trocar ou não pelo endereço de rede da máquina??
     #sock.bind(('localhost', 0))
     #host_address, host_port = sock.getsockname()
-    print "==> a porta escolhida foi {}".format(host_port)
+    logging.info("==> a porta escolhida foi {}".format(host_port))
     #print "==> e meu IP {}".format(host_address)
     sock.close()
 
